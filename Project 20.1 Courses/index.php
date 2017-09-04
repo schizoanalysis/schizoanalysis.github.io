57d2ca87417df88d900a0f7ea5203e1919afceb3
schizoanalysis <31521820+schizoanalysis@users.noreply.github.com>
2017-09-04 16:44:13 -0700

    yups

---
+++ Project 20.1 Courses/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action="index.php" method="post">
+      username:<br>
+      <input type="text" name="username"><br>
+      password
+      <input type="password" name="password">
+      <input type="submit" value="Login!">
+    </form>
+  </body>
+</html>
